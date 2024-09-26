5dfbd0a4bc5ca1e04cb2248cff2cb87456271570
cristiana.pereira <c.pereira@runtime-revolution.com>
2024-09-26 10:15:58 +0100

    feat: seed

--- db/test_seeds.rb
+++ db/test_seeds.rb
@@ -1,1 +1,12 @@
+
+User.destroy_all
+
 User.create(name: 'SuperAdmin', username: 'superAdmin', email: 'super.admin@gmail.com', password: 'veryStrongPassword')
+
+20.times do
+  User.create(
+    name: Faker::Fantasy::Tolkien.character,
+    username: Faker::Internet.username(specifier: 5..10),
+    email: Faker::Internet.email,
+    password: Faker::Internet.password)
+end
