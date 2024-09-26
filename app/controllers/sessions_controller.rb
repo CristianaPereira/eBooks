b02474adc53b41e0140f710b0c4c435e1c088dbd
cristiana.pereira <c.pereira@runtime-revolution.com>
2024-09-26 10:03:22 +0100

    fix: res status

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,23 +5,23 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params["email"].downcase).try(:authenticate, params["password"])
     if user
       session[:user_id] = user.id
-      render json: { status: :ok, logged_in: true, user: user }
+      render json: { logged_in: true, user: user }, status: :ok
     else
       # TODO: deal with error
-      render json: { status: 401 }
+      render json: {}, status: 401
     end
   end
 
   def logged_in
     if @current_user
-      render json: { logged_in: true, user: @current_user }
+      render json: { logged_in: true, user: @current_user }, status: :ok
     else
-      render json: { logged_in: false }
+      render json: { logged_in: false }, status: :ok
     end
   end
 
   def logout
     reset_session
-    render json: { status: 200, logged_out: true }
+    render json: { logged_in: false }, status: :ok
   end
 end
